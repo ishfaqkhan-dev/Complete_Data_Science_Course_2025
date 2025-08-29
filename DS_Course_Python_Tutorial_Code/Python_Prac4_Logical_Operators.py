e503eaa269d97809cde2cc8f2a239e4677d9aefe
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2025-08-29 12:20:15 -0800

    Done

---
+++ DS_Course_Python_Tutorial_Code/Python_Prac4_Logical_Operators.py
@@ -0,0 +1,24 @@
+
+# And Operator:
+
+# a > b and c > d
+
+# True + True = True
+# True + False = False
+# False + True = False
+# False + False = False
+
+
+# Or Operator:
+
+# True + False = True
+# True + True = True
+# False + True = True
+# False + False = False
+
+a = 10
+b = 20 
+c = 5
+d = 15
+
+print(not(a > b or c < d))
---
+++ DS_Course_Python_Tutorial_Code/Python_Prac5_Strings_Operations_And_Methods.py
@@ -0,0 +1,21 @@
+
+# text = "Python"
+
+# print(text[-3:])
+
+
+
+# print(len(text))
+# print(text.upper())
+# print(text.lower())
+# print(text.capitalize())
+# print(text[0])
+# print(text[-1])
+
+# first = "Ishfaq"
+# last = "Khan"
+
+# print(first + " " + last)
+
+a = "Hi"
+print(a * 3)
