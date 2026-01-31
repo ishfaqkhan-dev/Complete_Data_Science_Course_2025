ba64dc6105a13961fa31fb1adb0ba6e0e78378bc
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2026-01-31 02:08:00 -0800

    Done

---
+++ DS_Course_Python_Tutorial_Code/Python_Prac27_Polymorphism.py
@@ -0,0 +1,35 @@
+
+# class Cat:
+#     def sound(self):
+#         print("Meow")
+
+# class Dog:
+#     def sound(self):
+#         print("Bark")
+
+# def animal_sound(animal):
+#     animal.sound()
+
+# cat1 = Cat()
+# dog1 = Dog()
+
+# animal_sound(cat1)
+# animal_sound(dog1)
+
+# cat1.sound()
+# dog1.sound()
+
+# class person:
+#     def display(self):
+#         print("I am a person.")
+
+# class child(person):
+#     def display(self):
+#         print("I am a child")
+
+
+# p1 = person()
+# p1.display()
+
+# c1 = child()
+# c1.display()
